d12ab2cae738755b5d89fe68fca07c9e96610dc3
Ricardo Pereira <rjcpereira@outlook.pt>
2022-04-16 19:50:46 +0100

    logs

--- builder/handlebars.parse.js
+++ builder/handlebars.parse.js
@@ -2,6 +2,6 @@ const config = require('./config'),
     gulp = require('gulp'),
     babel = require('gulp-babel');
 
-module.exports = () => gulp.src(['dist/temp/**/*.js'])
+module.exports = () => gulp.src(['dist/temp/templates/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest(`${config.build.dest.web}/scripts/templates`));
--- builder/handlebars.precompile.js
+++ builder/handlebars.precompile.js
@@ -1,9 +1,12 @@
 const config = require('./config'),
+    colors  = require('colors'),
     shell = require('child_process').execSync;
 
 module.exports = ({ next, views }) => {
 
     shell(`mkdir -p ./${config.build.dest.temp}/templates`);
+
+    console.log(colors.red('TODO'), colors.yellow('minify html/hbs templates before'))
 
     for (let key in views) shell(`handlebars ${views[key].path} -f ./${config.build.dest.temp}/templates/${key}.js`);
 
