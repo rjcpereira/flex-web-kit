8ab157531a37bdba2a9ca9c76ad0fcd629cf71b2
Ricardo Pereira <rjcpereira@outlook.pt>
2022-04-17 18:56:41 +0100

    test

--- core/js/views.js
+++ core/js/views.js
@@ -1,18 +1,22 @@
-let views = Handlebars.templates || Handlebars.partials || {};
+window.addEventListener('load', () => {
 
-flex.views = new Proxy(views, {
-    get(target, prop) {
-        return target[prop];
-    },
-    set(target, prop, value) {
-        console.log(prop, value,Handlebars, Handlebars.templates, Handlebars.partials)
-        if(Handlebars) {
-            if(!Handlebars.templates) Handlebars.templates = {};
-            if(!Handlebars.partials) Handlebars.partials = {};
-            Handlebars.templates[prop] = Handlebars.partials[prop] = value;
+    let views = Handlebars.templates || Handlebars.partials || {};
+
+    flex.views = new Proxy(views, {
+        get(target, prop) {
+            return target[prop];
+        },
+        set(target, prop, value) {
+            console.log(prop, value, Handlebars, Handlebars.templates, Handlebars.partials)
+            if (Handlebars) {
+                if (!Handlebars.templates) Handlebars.templates = {};
+                if (!Handlebars.partials) Handlebars.partials = {};
+                Handlebars.templates[prop] = Handlebars.partials[prop] = value;
+            }
+            target[prop] = value;
+            console.warn(prop, value, Handlebars, Handlebars.templates, Handlebars.partials)
+            return true;
         }
-        target[prop] = value;
-        console.warn(prop, value,Handlebars, Handlebars.templates, Handlebars.partials)
-        return true;
-    }
-})
+    });
+
+});
