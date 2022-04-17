f1fc40d04b74c4d0dd29fc31031dccceb31ddc72
Ricardo Pereira <rjcpereira@outlook.pt>
2022-04-17 11:54:29 +0100

    dev

--- core/js/views.js
+++ core/js/views.js
@@ -3,12 +3,14 @@ flex.views = new Proxy({}, {
         return target[prop];
     },
     set(target, prop, value) {
-        target[prop] = value;
+        console.log(prop, value,Handlebars, Handlebars.templates, Handlebars.partials)
         if(Handlebars) {
             if(!Handlebars.templates) Handlebars.templates = {};
             if(!Handlebars.partials) Handlebars.partials = {};
             Handlebars.templates[prop] = Handlebars.partials[prop] = value;
         }
+        target[prop] = value;
+        console.warn(prop, value,Handlebars, Handlebars.templates, Handlebars.partials)
         return true;
     }
 })
