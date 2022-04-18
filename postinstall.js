7beb2d4b0de61629b76637b5ee4e402d7d9f0836
Ricardo Pereira <rjcpereira@outlook.pt>
2022-04-18 10:57:13 +0100

    10

--- postinstall.js
+++ postinstall.js
@@ -1,22 +1,3 @@
 const shell = require('child_process').exec;
 
-shell(`
-install=0
-
-for dir in \
-  "assets" \
-  "layouts" \
-  "pages"  \
-  "styles"  \
-  "pages" 
-do
-  if ! [ -d "${process.env.INIT_CWD}/$dir" ]; then
-    install=1
-    break
-  fi
-done
-
-if [ install == 1 ]; then
-  cp -r base/. ${process.env.INIT_CWD}
-fi
-`);
+shell(`cp -r base/. ${process.env.INIT_CWD}`);
